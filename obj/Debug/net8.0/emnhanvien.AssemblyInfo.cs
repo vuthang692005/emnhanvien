9197aee848642601cef1647a66c97e93515ced9f
unknown <vumitha2005@gmail.com>
2025-08-05 10:26:09 +0700

    sonarqube

--- obj/Debug/net8.0/emnhanvien.AssemblyInfo.cs
+++ obj/Debug/net8.0/emnhanvien.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("emnhanvien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9968646beefdd43e34b69761c4950aaef89c26b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a353bfe077233f4fa2d868523601de9a4f4ec52a")]
 [assembly: System.Reflection.AssemblyProductAttribute("emnhanvien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("emnhanvien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
