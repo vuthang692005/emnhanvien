5c2f6b5911f0926f496cf3c08d7271b9f6fcb2d0
unknown <vumitha2005@gmail.com>
2025-08-07 08:21:33 +0700

    check helth

---
+++ emnhanvien/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmNhanVien.Controllers
+{
+    [Route("health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
